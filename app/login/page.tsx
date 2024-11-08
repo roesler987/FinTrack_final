efd98549c1e2dd536fd397a4b1cd43ae6320f339
jeanrj987 <jeancarlosrj11@hotmail.com>
2024-11-07 22:04:26 -0400

    feat: add authentication

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,33 +5,36 @@ import { SignInButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-const LoginPage = () => {
+const LoginPage = async () => {
+  const { userId } = await auth();
+  if (userId) {
+    redirect("/");
+  }
   return (
     <div className="grid h-full grid-cols-2">
-      {/* {esquerda} */}
+      {/* ESQUERDA */}
       <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
         <Image
           src="/logo.svg"
           width={173}
           height={39}
-          alt="FinTrack"
+          alt="Finance AI"
           className="mb-8"
         />
         <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
-        <p className="text-muted-foreground">
+        <p className="mb-8 text-muted-foreground">
           A Finance AI é uma plataforma de gestão financeira que utiliza IA para
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
         </p>
-
-        <Button variant={"outline"}>
-            <LogInIcon className="mr-2">
-                Fazer Login ou criar conta
-            </LogInIcon>
-        </Button>
+        <SignInButton>
+          <Button variant="outline">
+            <LogInIcon className="mr-2" />
+            Fazer login ou criar conta
+          </Button>
+        </SignInButton>
       </div>
-
-      {/* {direita} */}
+      {/* DIREITA */}
       <div className="relative h-full w-full">
         <Image
           src="/login.png"
